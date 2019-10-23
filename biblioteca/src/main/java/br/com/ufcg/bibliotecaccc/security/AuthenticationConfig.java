1ea15a7940370a0493368c5903650b3b76d2b343
David <david.pereira@ccc.ufcg.edu.br>
2019-10-23 13:53:49 -0300

    ainda ta um pouco errado, mas o autorização de admin parace funcionar

---
+++ biblioteca/src/main/java/br/com/ufcg/bibliotecaccc/security/AuthenticationConfig.java
@@ -0,0 +1,9 @@
+package br.com.ufcg.bibliotecaccc.security;
+
+public class AuthenticationConfig {
+
+	public static final String ROLES = null;
+	public static final String HEADER = null;
+	public static final String TOKEN_PREFIX = null;
+
+}
